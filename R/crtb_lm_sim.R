8a6cfd16163baac03a0ff3cbe9bbd18798c6030b
Mackson Ncube <mightymetrika@proton.me>
2024-10-20 15:56:27 +0000

    add rejection rate to crtb_lm_sim

--- R/crtb_lm_sim.R
+++ R/crtb_lm_sim.R
@@ -16,6 +16,7 @@
 #'   Default is "Y ~ X1 + X2".
 #' @param tie_thresh Numeric. Threshold for deciding if there are too many ties
 #'   in the resampled data for CRTB. Default is 0.5.
+#' @param alpha Numeric. Alpha level for rejection rate and confidence intervals.
 #' @param progress Logical. If TRUE, prints progress updates. Default is FALSE.
 #'
 #' @return A data frame with the following columns:
@@ -25,6 +26,7 @@
 #'   \item{coverage}{Coverage probability of the confidence interval for beta1}
 #'   \item{ci_width}{Average width of the confidence interval for beta1}
 #'   \item{converged}{Proportion of CRTB resamples that converged (NA for bootstrap)}
+#'   \item{rejection}{Rejection rate}
 #'   \item{avg_jsd}{Average Jensen-Shannon Divergence for the resamples}
 #'
 #' @details
@@ -41,7 +43,7 @@
 #'
 #' @examples
 #' # Run a small simulation with default parameters
-#' sim_results <- crtb_lm_sim(n = 50, sim_iter = 5, B = 5)
+#' sim_results <- crtb_lm_sim(n = 50, sim_iter = 2, B = 2)
 #' print(sim_results)
 #'
 #' # Run a simulation with custom data generation
@@ -54,8 +56,8 @@
 #' }
 #' sim_results_custom <- crtb_lm_sim(
 #'   n = 100,
-#'   sim_iter = 5,
-#'   B = 5,
+#'   sim_iter = 2,
+#'   B = 2,
 #'   beta_gen = custom_beta_gen,
 #'   gen_ivs = custom_gen_ivs,
 #'   .formula = "Y ~ X1 + X2"
@@ -85,7 +87,12 @@ crtb_lm_sim <- function(n = 100, # Sample size
                         },
                         .formula = "Y ~ X1 + X2",
                         tie_thresh = 0.5,
+                        alpha = 0.05,
                         progress = FALSE){
+
+  if(B %% 2 != 0){
+    stop("must use even value for B due to the paired nature of crtb")
+  }
 
   # Extract response variable from the formula
   formula_vars <- all.vars(stats::as.formula(.formula))
@@ -143,6 +150,7 @@ crtb_lm_sim <- function(n = 100, # Sample size
       beta1_CI_low = numeric(sim_iter),
       beta1_CI_high = numeric(sim_iter),
       beta1_coverage = numeric(sim_iter),
+      beta1_reject = numeric(sim_iter),
       jsd = numeric(sim_iter)
     ),
     crtb = list(
@@ -153,6 +161,7 @@ crtb_lm_sim <- function(n = 100, # Sample size
       beta1_CI_low = numeric(sim_iter),
       beta1_CI_high = numeric(sim_iter),
       beta1_coverage = numeric(sim_iter),
+      beta1_reject = numeric(sim_iter),
       jsd = numeric(sim_iter)
     ),
     original_beta1 = numeric(sim_iter)
@@ -188,12 +197,14 @@ crtb_lm_sim <- function(n = 100, # Sample size
 
     # Bootstrap Resampling
     beta1_bootstrap <- numeric(B)
+    beta1_rejection <- numeric(B)
     data_bootstrap_list <- vector("list", B)
     for (b in 1:B) {
       indices <- sample(1:n, size = n, replace = TRUE)
       data_bootstrap <- data[indices, ]
       model_bootstrap <- stats::lm(stats::as.formula(.formula), data = data_bootstrap)
       beta1_bootstrap[b] <- stats::coef(model_bootstrap)[predictor_vars[1]]
+      beta1_rejection[b] <- summary(model_bootstrap)$coefficients[predictor_vars[1], "Pr(>|t|)"] < alpha
       data_bootstrap_list[[b]] <- data_bootstrap
     }
 
@@ -205,8 +216,8 @@ crtb_lm_sim <- function(n = 100, # Sample size
     beta1_bias_bootstrap <- mean(beta1_bootstrap) - betas[[2]]
 
     # 95% confidence interval
-    beta1_CI_low_bootstrap <- beta1_hat - stats::qnorm(0.975) * beta1_SE_bootstrap
-    beta1_CI_high_bootstrap <- beta1_hat + stats::qnorm(0.975) * beta1_SE_bootstrap
+    beta1_CI_low_bootstrap <- beta1_hat - stats::qnorm(1 - alpha/2) * beta1_SE_bootstrap
+    beta1_CI_high_bootstrap <- beta1_hat + stats::qnorm(1 - alpha/2) * beta1_SE_bootstrap
 
     beta1_coverage_bootstrap <- (beta1_CI_low_bootstrap <= betas[[2]]) & (beta1_CI_high_bootstrap >= betas[[2]])
 
@@ -217,6 +228,7 @@ crtb_lm_sim <- function(n = 100, # Sample size
     results$bootstrap$beta1_CI_low[sim] <- beta1_CI_low_bootstrap
     results$bootstrap$beta1_CI_high[sim] <- beta1_CI_high_bootstrap
     results$bootstrap$beta1_coverage[sim] <- beta1_coverage_bootstrap
+    results$bootstrap$beta1_reject[sim] <- mean(beta1_rejection)
 
     # Compute JSD for bootstrap resamples
     bootstrap_resamples <- results$bootstrap$resample[[sim]]
@@ -224,6 +236,7 @@ crtb_lm_sim <- function(n = 100, # Sample size
 
     # CRTB
     beta1_crtb <- numeric(B)
+    beta1_crtb_rejection <- numeric(B)
     resamples_list <- vector("list", B)
     b_crtb_counter <- 1
     crtb_converged <- 0
@@ -241,6 +254,7 @@ crtb_lm_sim <- function(n = 100, # Sample size
       ordat <- data[indices$ordat, ]
       model_crtb_ordat <- stats::lm(stats::as.formula(.formula), data = ordat)
       beta1_crtb[b_crtb_counter] <- stats::coef(model_crtb_ordat)['X1']
+      beta1_crtb_rejection[b_crtb_counter] <- summary(model_crtb_ordat)$coefficients[predictor_vars[1], "Pr(>|t|)"] < alpha
       b_crtb_counter <- b_crtb_counter + 1
       resamples_list[[b_crtb_counter]] <- ordat
 
@@ -248,6 +262,7 @@ crtb_lm_sim <- function(n = 100, # Sample size
       crdat <- data[indices$crdat, ]
       model_crtb_crdat <- stats::lm(stats::as.formula(.formula), data = crdat)
       beta1_crtb[b_crtb_counter] <- stats::coef(model_crtb_crdat)['X1']
+      beta1_crtb_rejection[b_crtb_counter] <- summary(model_crtb_crdat)$coefficients[predictor_vars[1], "Pr(>|t|)"] < alpha
       b_crtb_counter <- b_crtb_counter + 1
       resamples_list[[b_crtb_counter]] <- crdat
     }
@@ -266,8 +281,8 @@ crtb_lm_sim <- function(n = 100, # Sample size
     beta1_bias_crtb <- mean(beta1_crtb) - betas[[2]]
 
     # 95% confidence interval
-    beta1_CI_low_crtb <- beta1_hat - stats::qnorm(0.975) * beta1_SE_crtb
-    beta1_CI_high_crtb <- beta1_hat + stats::qnorm(0.975) * beta1_SE_crtb
+    beta1_CI_low_crtb <- beta1_hat - stats::qnorm(1 - alpha/2) * beta1_SE_crtb
+    beta1_CI_high_crtb <- beta1_hat + stats::qnorm(1 - alpha/2) * beta1_SE_crtb
 
     beta1_coverage_crtb <- (beta1_CI_low_crtb <= betas[[2]]) & (beta1_CI_high_crtb >= betas[[2]])
 
@@ -282,6 +297,7 @@ crtb_lm_sim <- function(n = 100, # Sample size
     results$crtb$beta1_CI_low[sim] <- beta1_CI_low_crtb
     results$crtb$beta1_CI_high[sim] <- beta1_CI_high_crtb
     results$crtb$beta1_coverage[sim] <- beta1_coverage_crtb
+    results$crtb$beta1_reject[sim] <- mean(beta1_crtb_rejection)
 
     # Compute JSD for CRTB resamples
     crtb_resamples <- results$crtb$resample[[sim]]
@@ -309,6 +325,9 @@ crtb_lm_sim <- function(n = 100, # Sample size
   bootstrap_CI_width_mean <- mean(results$bootstrap$beta1_CI_high - results$bootstrap$beta1_CI_low, na.rm = TRUE)
   crtb_CI_width_mean <- mean(results$crtb$beta1_CI_high - results$crtb$beta1_CI_low, na.rm = TRUE)
 
+  bootstrap_rejection_mean <- mean(results$bootstrap$beta1_reject,na.rm = TRUE)
+  crtb_rejection_mean <- mean(results$crtb$beta1_reject, na.rm = TRUE)
+
   # Output the results
   output <- data.frame(results= c("bootstrap",
                                   "crtb"),
@@ -320,8 +339,10 @@ crtb_lm_sim <- function(n = 100, # Sample size
                                     crtb_coverage_mean),
                        ci_width = c(bootstrap_CI_width_mean,
                                     crtb_CI_width_mean),
-                       converged = c(NULL,
+                       converged = c(NA,
                                      crtb_converged/B_crtb),
+                       rejection = c(bootstrap_rejection_mean,
+                                     crtb_rejection_mean),
                        avg_jsd = c(mean(results$bootstrap$jsd), mean(results$crtb$jsd)))
 
   return(output)

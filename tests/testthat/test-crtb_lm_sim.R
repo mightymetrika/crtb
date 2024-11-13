770b3b4071bd893bd95271e19442bee87ec6d7be
Mackson Ncube <mightymetrika@proton.me>
2024-11-13 15:45:03 +0000

    remove parallel computing aspect

--- tests/testthat/test-crtb_lm_sim.R
+++ tests/testthat/test-crtb_lm_sim.R
@@ -1,4 +1,4 @@
 test_that("crtb_lm_sim works", {
-  simres <- crtb_lm_sim(sim_iter = 2, B = 2, tie_thresh = 0) |> suppressWarnings()
+  simres <- crtb_lm_sim(sim_iter = 20, B = 2, tie_thresh = 0) |> suppressWarnings()
   expect_s3_class(simres, "data.frame")
 })

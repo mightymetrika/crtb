d1a1a0e42c1463b1f29e47b7bbe64180e9d05856
Mackson Ncube <mightymetrika@proton.me>
2024-11-26 18:16:55 +0000

    run pow_2var_cor_n15_sim

--- R/data.R
+++ R/data.R
@@ -361,3 +361,20 @@
 #'   \item{rejection}{Rejection rate}
 #' }
 "pow_2var_uncor_n30_wide_sim"
+
+#' Simulation Results: pow_2var_cor_n15_sim
+#'
+#' Results from simulation study
+#'
+#' @format ## `pow_2var_cor_n15_sim`
+#' A data frame with 2 rows and 7 columns:
+#' \describe{
+#'   \item{results}{Bootstrap method}
+#'   \item{avg_bias}{Average bias across simulation iterations}
+#'   \item{avg_se}{Standard error across simulation iterations}
+#'   \item{coverage}{Proportion of confidence intervals that include true parameter}
+#'   \item{ci_width}{Average confidence interval width}
+#'   \item{converged}{Proportion of converged crtb}
+#'   \item{rejection}{Rejection rate}
+#' }
+"pow_2var_cor_n15_sim"

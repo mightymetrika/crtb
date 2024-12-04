58b8cd44519a603271ed6c415f607870b0e1b081
Mackson Ncube <mightymetrika@proton.me>
2024-12-04 22:27:10 +0000

    run type1_2var_uncor_n150_wide_het

--- R/data.R
+++ R/data.R
@@ -582,3 +582,20 @@
 #'   \item{rejection}{Rejection rate}
 #' }
 "type1_2var_uncor_n75_wide_het"
+
+#' Simulation Results: type1_2var_uncor_n150_wide_het
+#'
+#' Results from simulation study
+#'
+#' @format ## `type1_2var_uncor_n150_wide_het`
+#' A data frame with 2 rows and 7 columns:
+#' \describe{
+#'   \item{results}{Bootstrap method}
+#'   \item{avg_bias}{Average bias across simulation iterations}
+#'   \item{avg_se}{Standard error across simulation iterations}
+#'   \item{coverage}{Proportion of confidence intervals that include true parameter}
+#'   \item{ci_width}{Average confidence interval width}
+#'   \item{converged}{Proportion of converged crtb}
+#'   \item{rejection}{Rejection rate}
+#' }
+"type1_2var_uncor_n150_wide_het"
